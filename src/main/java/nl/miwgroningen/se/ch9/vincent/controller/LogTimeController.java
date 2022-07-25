546b30e087a7d55f7cfbef0e8e70d822dafa5a4d
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-07-25 10:15:09 +0200

    Change text on button

--- src/main/java/nl/miwgroningen/se/ch9/vincent/controller/LogTimeController.java
+++ src/main/java/nl/miwgroningen/se/ch9/vincent/controller/LogTimeController.java
@@ -2,6 +2,7 @@ package nl.miwgroningen.se.ch9.vincent.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import nl.miwgroningen.se.ch9.vincent.model.TimeLog;
@@ -13,19 +14,24 @@ import nl.miwgroningen.se.ch9.vincent.model.TimeLog;
  */
 public class LogTimeController {
 
+
     private TimeLog timeLog = null;
     @FXML
     private TextField eventField;
+    @FXML
+    private Button toggleLog;
     @FXML
     private Label previousLog;
 
     public void toggleLog(ActionEvent actionEvent) {
         if (timeLog == null) {
             timeLog = new TimeLog();
+            toggleLog.setText("Stop Log");
         } else {
             timeLog.endLog(eventField.getText());
             previousLog.setText(timeLog.toString());
             timeLog = null;
+            toggleLog.setText("Start Log");
         }
     }
 }
